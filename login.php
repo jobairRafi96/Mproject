e4cbf388d60fb318e42ce7574768e8f8065c3827
jobairRafi96 <60178238+jobairRafi96@users.noreply.github.com>
2020-03-06 05:20:57 +0600

    common feature

---
+++ login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+	<form method="POST" action="logincheck.php">
+
+			<table align="center">
+				<tr>
+					<td colspan="3" align="center"><h1>Login</h1></td>
+				</tr>
+
+				<tr>
+					<td colspan="3"></td>
+				</tr>
+
+				<tr>
+					<td>User Name</td>
+					<td>:</td>
+					<td><input type="text" name="uname" value="" size="40" placeholder="Name"></td>
+				</tr>
+
+				<tr>
+					<td colspan="3"></td>
+				</tr>
+
+				<tr>
+					<td>Password</td>
+					<td>:</td>
+					<td><input type="Password" name="pass" value="" size="40" placeholder="Password"></td>
+				</tr>
+
+				<tr>
+					<td colspan="3"></td>
+				</tr>
+
+				<tr>
+					<td>User Type</td>
+					<td>:</td>
+					<td><input type="radio" name="utype" value="owner">Owner
+						<input type="radio" name="utype" value="manager">Manager
+						<input type="radio" name="utype" value="chef">Chef
+						<input type="radio" name="utype" value="guest">Guest</td>
+				</tr>
+
+				<tr>
+					<td colspan="3"></td>
+				</tr>
+
+				<tr>
+					<td colspan="3" align="right"><input type="submit" name="submit" value="Login"></td>
+				</tr>
+			</table>
+
+	</form>
+
+</body>
+</html>
