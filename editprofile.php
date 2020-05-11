9033e4b81098d3b88e407920f894a3904962e158
jobairRafi96 <jubairrafi96960@gmail.com>
2020-05-11 06:01:57 -0700

    profile error msg with js

--- editprofile.php
+++ editprofile.php
@@ -12,6 +12,27 @@
 <head>
 	<title>Profile</title>
 </head>
+<style media="screen">
+  body{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  .errorMsg{
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #d63447;
+    color: white;
+    font-family: fantasy;
+    width: 100vh;
+    height: 50px;
+    font-size: 1rem;
+    left: 380px;
+    top: 400px;
+  }
+</style>
 <body>
 
 	<table align="center">
@@ -28,6 +49,10 @@
 
 <fieldset>
 	<legend>Profile</legend>
+  <div class="msg">
+<!-- <h3>at least 5 character for password</h3> -->
+  </div>
+
   <form id="editprofileValidation" action="php/editcheck.php" method="post" enctype="multipart/form-data">
     <table align="center">
 
@@ -91,46 +116,35 @@
       </tr>
 
   		<tr>
-  			<td colspan="3"></td>
-  		</tr>
-  		<tr>
-  			<td colspan="3"></td>
-  		</tr>
-  		<tr>
-  			<td colspan="3"></td>
-  		</tr>
-  		<tr>
-  			<td colspan="3"></td>
-  		</tr>
-  		<tr>
-  			<td colspan="3"></td>
-  		</tr>
-
-
-  		<tr>
-  			<td colspan="3"></td>
-  		</tr>
-  		<tr>
-  			<td colspan="3"></td>
-  		</tr>
-  		<tr>
-  			<td colspan="3"></td>
-  		</tr>
-  		<tr>
-  			<td colspan="3"></td>
-  		</tr>
-  		<tr>
-  			<td colspan="3"></td>
-  		</tr>
-
-  		<tr>
   			<td colspan="3" align="right"><input type="button"  value="Done" onclick="editprofileValidation()"></td>
   		</tr>
 
   	</table>
   </form>
 
-  <script src="index.js" charset="utf-8"></script>
+  <script charset="utf-8">
+
+  function editprofileValidation(){
+    var pass = document.getElementById("cpass").value;
+    var msgDiv = document.querySelector(".msg");
+    var msgDivclass = document.querySelector(".msg").classList;
+        if (pass.length<5) {
+            msgDivclass.add("errorMsg");
+            msgDiv.innerHTML="<h3>password must contain at least 5 charecter</h3>";
+
+                setTimeout(function(){
+                  msgDivclass.remove("errorMsg");
+                  msgDiv.innerHTML="";
+                },2500);
+
+            return false;
+        } else {
+            document.getElementById("editprofileValidation").submit();
+        }
+  }
+
+
+  </script>
 
 
 </body>
